5fa5a8c7fd4d98fa2368348ff1eeceb00169fb1c
Ubuntu12043 <sun_fan0825@sina.com>
2016-04-05 19:33:28 +0800

    s

--- python/SpiderRentals/code.py
+++ python/SpiderRentals/code.py
@@ -4,13 +4,50 @@ import urllib2
 import re
 import sys
 
+
+class LocationType(object):
+	"""docstring for Urls"""
+
+	type_name =  ''
+	url = ''
+	areas = dict()
+	url_count = 0
+
+	def __init__(self, type_name):
+		
+		self.type_name = type_name
+
+	def add_area(area_name, area):
+		if area_name not in areas:
+			areas[area_name] = area
+			url_count += 1
+
+	def get_area():
+		return areas
+
+class Area(object):
+	"""docstring for Area"""
+
+	name = ''
+	url = ''
+	locations = list()
+	location_count = 0
+
+	def __init__(self, name):
+		self.name = name
+		
+	def add_location(location):
+
+		locations.append(location)
+		location_count += 1
+
+	def get_location():
+		return locations
+
 class Location:
 
 	name = ''
 	url = ''
-	area = ''
-	type_name = ''
-	sub = ''
 
 	def __init__(self, name, url):
 		self.name = name
@@ -42,17 +79,24 @@ def analyze():
 	
 	type_p = re.compile(r'<dt>(?P<type_name>.*)：</dt>')
 	area_p = re.compile(r'<span class="tag"><a href="(?P<url>.*)">(?P<area>.*)</a') 
+	location_p = re.compile(r'<a href="(?P<url>.*)" >(?P<location>.*)</a>')
 	search_type = dict()
 	type_name = ''
+	area_name = ''
+	location_name = ''
+	LocationType locations = LocationType('')
+	urls = list()
+
 	for data in datas:
 		type_match = type_p.search(data)
 		area_match = area_p.search(data)
+		location_match = location_p.search(data)
 		if type_match:
 			type_name = type_match.group('type_name')
-			search_type[type_name] = dict()
-		if area_match:
+		elif area_match:
 			area_name = area_match.group('area')
-			search_type[type_name][area_name] = list()
+		elif location_match:
+
 
 	return search_type
 
