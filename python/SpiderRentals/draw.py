d14c97d1920910ad2d7e8cda26b7e91451caec5c
fan.sun <fan.sun@corp.elong.com>
2016-05-25 10:09:36 +0800

    update

--- python/SpiderRentals/draw.py
+++ python/SpiderRentals/draw.py
@@ -28,7 +28,15 @@ def test2():
 	y = [1, 4, 9, 16, 25]
 
 	#pl.plot(x, y)
+	#pl.title('坐标系')
+	pl.title('title')
 	pl.plot(x, y, 'c*')
+
+	pl.xlabel('x axis')
+	pl.ylabel('y axis')
+	pl.xlim(0.0, 9.0)
+	pl.ylim(0.0, 30)
+
 	pl.show()
 
 def test1():
@@ -47,5 +55,25 @@ def test1():
 def test3():
 	scatterplot([1,3,1,2,3],[2,5,4,4,3])
 
+def test4():
+
+	plt.figure(figsize=(8,6), dpi=80)
+	plt.subplot(111)
+
+	x = np.linspace(-np.pi, np.pi, 256, endpoint=True)
+	c,s = np.cos(x), np.sin(x)
+
+	plt.plot(x, c, color='blue', linewidth=1.0, linestyle='-')
+	plt.plot(x, s, color='green', linewidth=1.0, linestyle='-')
+
+	plt.xlim(-4.0, 4.0)
+	
+
+	plt.show()
+
 if __name__ == '__main__':
-	test3()
+	try:
+		test4()
+	except Exception, e:
+		print e
+	
